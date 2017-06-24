eda9dc3b746d37fb800afeb05a028d57ab00fdd9
ganben <caozhao@gmail.com>
2017-06-24 14:00:20 +0800

    add module

---
+++ main.py
@@ -0,0 +1,34 @@
+#encoding=utf-8
+#by ganben
+#this is a demostrate python program entrance script
+# the goal of this program is (1) log into a weibo account, 
+# and (2) refresh the public weibo timeline, (3) parse the responsed text
+# and (4) store in mongo db. the four feature is divided into 4 modules
+# and the main process is a timed thread.
+import urllib3
+import weibo
+
+def main():
+    #init
+    #connect to db and retrieve saved job, parameter
+    #login and store token
+    #start loop
+    while True:
+        #refresh
+        urllib3.connection
+        weibo.weibo.login
+        #parse
+
+        #save
+
+        #sleep time
+
+        pass
+    #
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
+
