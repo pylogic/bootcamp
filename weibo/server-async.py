35054f2257a7922e7706417068c0511679881529
ganben <caozhao@gmail.com>
2017-09-25 21:02:36 +0800

    add responsive vue front-end

--- weibo/server-async.py
+++ weibo/server-async.py
@@ -8,6 +8,7 @@ from flask import request
 from flask import session
 from flask import url_for
 from flask import jsonify
+import puer.puer as puer
 import logging
 import textprocess.scchtrans as ts
 from cachetools import LRUCache
@@ -16,8 +17,18 @@ import configparser
 
 cache = LRUCache(maxsize=100)
 
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+    block_start_string='(%',
+    block_end_string='%)',
+    variable_start_string='((',
+    variable_end_string='))',
+    comment_start_string='(#',
+    comment_end_string='#)',
+  ))
 
-app = Flask(__name__)
+app = CustomFlask(__name__)
 
 # class CustomFlask(Flask):
 # jinja_options = Flask.jinja_options.copy()
@@ -34,7 +45,7 @@ config = configparser.ConfigParser()
 setting = config.read('config.ini')
 # print('setting=%s' % config['SectionOne']['Param1'] )
 HOST = '0.0.0.0'
-PORT = 8080
+PORT = 8010
 MODE = 'dev'
 try:
     HOST = config['Server']['HOST']
@@ -87,7 +98,7 @@ def encrypt():
         keychar = ts.filterchars(key)
 
         if len(chars)>0 and len(keychar) >0:
-           ecr = ts.encryptext(chars, keychar)
+           ecr = puer.encrypt(key, text)
            return jsonify({'key': keychar, 'text': ecr})
         else:
             return jsonify({'error': 'no valid chars'})
@@ -96,7 +107,38 @@ def encrypt():
     else:
         return jsonify({'error': 'too long text or key'})
 
-
+@app.route('/api', methods=['POST'])
+def api():
+    """only for fd api"""
+    
+    try:
+        incoming = request.get_json()
+        key = incoming.get('key')
+        text = incoming.get('text')
+        if 0 < len(text) <= 200 and 0 < len(key)<=8:
+            chars = ts.filterchars(text)
+            keychar = ts.filterchars(key)
+            if len(chars) == 0 or len(keychar) == 0:
+                return jsonify({'error': 'invalid chars'})
+        else:
+            return jsonify({'error': 'too long chas'})
+        
+    except:
+        return jsonify({'error': 'no json data get'})
+    if incoming.get('mode') == 'enc':
+        ecr = puer.encrypt(key, text)
+        return jsonify({'encrypted': True,
+                              'res':ecr})
+    elif incoming.get('mode') == 'dec':
+        dcr = puer.decrypt(key, text)
+        return jsonify({'decrypted': True,
+                         'res': dcr
+                         })
+    else:
+        return jsonify({'error': 'mission mode'})
+    
+    return jsonify({'res': 'ok'})
+    
 
 @app.route('/dec', methods=['GET', 'POST'])
 def decrypt():
